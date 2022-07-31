30a9034700dd82d4fe880fbbd59cc98ce77d8816
Leo <2108686658@qq.com>
2022-07-31 16:20:05 +0800

    hot-fix commit2

--- sca-common/src/main/java/com/jt/common/cache/testCache.java
+++ sca-common/src/main/java/com/jt/common/cache/testCache.java
@@ -46,10 +46,17 @@ public class testCache {
                 System.out.println(selectAll());
             }
         };
+        Thread t5= new Thread(){
+            @Override
+            public void run() {
+                System.out.println(selectAll());
+            }
+        };
         t1.start();
         t2.start();
         t3.start();
         t4.start();
+        t5.start();
 
 
     }
